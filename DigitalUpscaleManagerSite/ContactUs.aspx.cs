3580a87469979fc02b59387412b6e99af5444994
Osayame Gaius <osayame@gmail.com>
2015-02-27 21:10:23 -0500

    Fixed Resharper errors. Removed url from Site Map node in web.sitemap

--- DigitalUpscaleManagerSite/ContactUs.aspx.cs
+++ DigitalUpscaleManagerSite/ContactUs.aspx.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
+/// <summary>
+/// Code behind for the ContactUs page.
+/// </summary>
+/// <author>Osa Gaius</author>
+/// <version>Spring 2015</version>
 public partial class ContactUs : System.Web.UI.Page
 {
+    /// <summary>
+    /// Handles the Load event of the Page control.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
     protected void Page_Load(object sender, EventArgs e)
     {
 
