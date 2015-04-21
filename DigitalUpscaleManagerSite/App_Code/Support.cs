d3a532a749f0106e3a75d5ae3f6cc501fbf3b6b2
Osayame Gaius-Obaseki <osayame@gmail.com>
2015-04-20 22:39:04 -0400

    Added FeedbackBySupport page and Support class.

---
+++ DigitalUpscaleManagerSite/App_Code/Support.cs
@@ -0,0 +1,105 @@
+﻿using System.Diagnostics;
+
+
+/// <summary>
+/// Summary description for Support
+/// </summary>
+public class Support
+{
+    #region Instance Variables
+
+    /// <summary>
+    /// The support identifier
+    /// </summary>
+    private string _supportId;
+    /// <summary>
+    /// The support name
+    /// </summary>
+    private string _name;
+    /// <summary>
+    /// The email
+    /// </summary>
+    private string _email;
+    /// <summary>
+    /// The phone
+    /// </summary>
+    private string _phone;
+
+    #endregion
+
+    #region Methods
+
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the support identifier.
+    /// </summary>
+    /// <value>
+    /// The support identifier.
+    /// </value>
+    public string SupportId
+    {
+        get { return this._supportId; }
+        set
+        {
+            Trace.Assert(value != null);
+            this._supportId = value;
+        }
+    }
+
+
+    /// <summary>
+    /// Gets or sets the name.
+    /// </summary>
+    /// <value>
+    /// The name.
+    /// </value>
+    public string Name
+    {
+        get { return this._name; }
+        set
+        {
+            Trace.Assert(value != null);
+            this._name = value;
+        }
+    }
+
+
+    /// <summary>
+    /// Gets or sets the email.
+    /// </summary>
+    /// <value>
+    /// The email.
+    /// </value>
+    public string Email
+    {
+        get { return this._email; }
+        set
+        {
+            Trace.Assert(value != null);
+            this._email = value;
+        }
+    }
+
+=
+    /// <summary>
+    /// Gets or sets the phone.
+    /// </summary>
+    /// <value>
+    /// The phone.
+    /// </value>
+    public string Phone
+    {
+        get { return this._phone; }
+        set
+        {
+            Trace.Assert(value != null);
+            this._phone = value;
+        }
+    }
+
+    #endregion
+}
---
+++ DigitalUpscaleManagerSite/FeedbackBySupport.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/DigitalUpscale.master" CodeFile="FeedbackBySupport.aspx.cs" Inherits="FeedbackBySupport" %>
+
+<asp:Content ID="headContent" ContentPlaceHolderID="headPlaceHolder" runat="server">
+</asp:Content>
+<asp:Content ID="formContent" ContentPlaceHolderID="formPlaceHolder" runat="server">
+    <br />
+    <asp:DropDownList ID="ddlStaff" runat="server" DataSourceID="sdsSupport" DataTextField="Name" DataValueField="SupportID" AutoPostBack="True">
+    </asp:DropDownList>
+
+    <asp:ObjectDataSource ID="ObjectDataSource1" runat="server"></asp:ObjectDataSource>
+</asp:Content>
